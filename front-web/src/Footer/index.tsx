3ec76a17c0aabd8e32fd5934172e88ba5b689856
unknown <ynogueiira@gmail.com>
2021-01-10 22:23:49 -0300

    inclui os hrefs do footer, tinha dado erro no netlify

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -8,13 +8,13 @@ function Footer() {
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a target="_new" href="">
+                <a target="_new" href="https://www.youtube.com/watch?v=Wl959QnD3lM&t=45s&ab_channel=lerrific">
                     <YouTubeIcon />
                 </a>
-                <a target="_new" href="">
+                <a target="_new" href="https://www.linkedin.com/in/yuri-ferreira-a38b26203/">
                     <LinkedinIcon />
                 </a>
-                <a target="_new" href="">
+                <a target="_new" href="https://www.instagram.com/yferreiira/">
                     <InstagramIcon />
                 </a>
             </div>

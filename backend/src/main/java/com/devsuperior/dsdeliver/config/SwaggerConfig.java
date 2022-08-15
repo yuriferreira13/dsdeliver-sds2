995ee8ef6d9fb6c6fa9549d997c9424c498ae7c0
Yuri Ferreira <ynogueiira@gmail.com>
2022-08-15 17:31:10 -0300

    Adicionando Swagger para documentar a API
    Visualiza http://localhost:8080/swagger-ui/

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dsdeliver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.devsuperior.dsdeliver.controllers"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(buildApiInfo());
+	}
+	
+	private ApiInfo buildApiInfo() {
+		return new ApiInfoBuilder()
+				.title("API  DSdeliver")
+				.description("REST API DSdeliver ")
+				.version("1.0.0")
+				.contact(new Contact("Yuri Ferreira", "github.com/yuriferreira13", null))
+				.build();
+	}
+
+}
